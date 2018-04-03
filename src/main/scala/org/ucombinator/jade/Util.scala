99d1a1ed788be1670095d9cbcd6aa43902ad3308
Michael D. Adams <https://michaeldadams.org/>
2018-04-03 15:31:35 -0600

    Add `Util.scala`

---
+++ src/main/scala/org/ucombinator/jade/Util.scala
@@ -0,0 +1,51 @@
+package org.ucombinator.jade
+
+import org.jgrapht.{Graph, Graphs}
+
+import scala.collection.immutable
+import scala.collection.JavaConverters._
+
+class Util {
+  // Returns a mapping from nodes to the set of nodes that dominate them
+  def dominators[V,E](graph: Graph[V,E], start: V): immutable.Map[V, immutable.Set[V]] = {
+    val vs = graph.vertexSet.asScala.toSet
+    var dom: immutable.Map[V, immutable.Set[V]]  = Map.empty
+
+    // Initial assignment
+    dom = dom + (start -> immutable.Set(start))
+    for (v <- vs if v != start) {
+      dom = dom + (v -> vs)
+    }
+
+    // Iteration until fixed point
+    var old_dom = dom
+    do {
+      old_dom = dom
+      for (v <- vs if v != start) {
+        dom = dom +
+          (v -> (
+            immutable.Set(v) ++
+              Graphs.predecessorListOf(graph, v).
+                asScala.map(dom).
+                fold(vs)(_ & _)))
+      }
+    } while (old_dom != dom)
+
+    return dom
+  }
+
+  // Returns a mapping from nodes to the node that is its immediate dominator
+  def immediateDominators[V,E](graph: Graph[V,E], start: V): immutable.Map[V, V] = {
+    val dom = dominators(graph, start)
+
+    object O extends Ordering[V] {
+      override def compare(x: V, y: V): Int = {
+        if (dom(x)(y)) -1
+        else if (dom(y)(x)) 1
+        else 0
+      }
+    }
+
+    dom.mapValues(_.min(O))
+  }
+}
