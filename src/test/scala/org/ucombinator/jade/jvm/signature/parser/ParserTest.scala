2b494c0d85dca113f8c96334c01c02fc383fbb56
Michael D. Adams <https://michaeldadams.org/>
2018-04-03 23:57:27 -0600

    Refactoring the signature parser

--- src/test/scala/org/ucombinator/jade/jvm/signature/parser/ParserTest.scala
+++ src/test/scala/org/ucombinator/jade/jvm/signature/parser/ParserTest.scala
@@ -1,9 +1,7 @@
 package org.ucombinator.jade.jvm.signature.parser
 
 import org.scalatest.FunSuite
-
 import org.ucombinator.jade.jvm.signature.Signature._
-
 
 class ParserTest extends FunSuite {
   test("A class signature after parsing should be equal to the result we manually construct") {
@@ -11,31 +9,25 @@ class ParserTest extends FunSuite {
     val result = ClassSignature(
       List(
         TypeParameter("K",
-          Some(ClassTypeSignature(List("java", "lang"),List(SimpleClassTypeSignature("Object", Nil)))),
+          Some(ClassTypeSignature(List("java", "lang"), List(SimpleClassTypeSignature("Object", Nil)))),
           Nil),
         TypeParameter("V",
-          Some(ClassTypeSignature(List("java", "lang"),List(SimpleClassTypeSignature("Object",Nil)))),
+          Some(ClassTypeSignature(List("java", "lang"), List(SimpleClassTypeSignature("Object", Nil)))),
           Nil)),
       ClassTypeSignature(
         List("java", "util"),
         List(SimpleClassTypeSignature("AbstractMap",
-          List(BoundedTypeArgument(None,TypeVariableSignature("K")),
-               BoundedTypeArgument(None,TypeVariableSignature("V")))))),
+          List(BoundedTypeArgument(None, TypeVariableSignature("K")),
+               BoundedTypeArgument(None, TypeVariableSignature("V")))))),
       List(
         ClassTypeSignature(List("java", "util"),
-          List(SimpleClassTypeSignature("Map",List(
-            BoundedTypeArgument(None,TypeVariableSignature("K")),
-            BoundedTypeArgument(None,TypeVariableSignature("V")))))),
+          List(SimpleClassTypeSignature("Map",
+            List(BoundedTypeArgument(None, TypeVariableSignature("K")),
+                 BoundedTypeArgument(None, TypeVariableSignature("V")))))),
         ClassTypeSignature(List("java", "lang"),
-          List(SimpleClassTypeSignature("Cloneable",Nil))),
+          List(SimpleClassTypeSignature("Cloneable", Nil))),
         ClassTypeSignature(List("java", "io"),
-          List(SimpleClassTypeSignature("Serializable",Nil)))))
+          List(SimpleClassTypeSignature("Serializable", Nil)))))
     assert(Parser.parseClassSignature(classSignature) == result)
   }
-
-//  test("Invoking head on an empty Set should produce NoSuchElementException") {
-//    assertThrows[NoSuchElementException] {
-//      Set.empty.head
-//    }
-//  }
 }
