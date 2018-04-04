09aa1dae0e6cb6445dff9169b1fa83e0a1541bc9
Michael D. Adams <https://michaeldadams.org/>
2018-04-03 20:01:30 -0600

    More work on parsers

--- src/main/scala/org/ucombinator/jade/jvm/signature/parser/TMethodSignatureParser.scala
+++
@@ -1,8 +0,0 @@
-package org.ucombinator.jade.jvm.signature.parser
-
-import org.ucombinator.jade.jvm.signature.Signature._
-
-
-trait TMethodSignatureParser extends TJavaTypeSignatureParser with TTypeParameterParser {
-}
-
