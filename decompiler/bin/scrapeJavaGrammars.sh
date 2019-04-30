22d758448ab62ef51bd889a201194c97b0849c0b
Michael D. Adams <https://michaeldadams.org/>
2019-04-30 12:21:28 -0600

    Remove old AST generation code

--- decompiler/bin/scrapeJavaGrammars.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-#wget https://docs.oracle.com/javase/specs/jls/se8/html/index.html
-
-php ./bin/extractGrammarFromHTML.php "https://docs.oracle.com/javase/specs/jls/se8/html/jls-19.html"
-
