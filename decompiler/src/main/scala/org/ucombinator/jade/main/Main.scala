553698ea9e474b020cb5096358e0ef24b7ac5cd4
Michael D. Adams <https://michaeldadams.org/>
2019-05-15 14:12:36 -0600

    Refactor command line parsing

--- decompiler/src/main/scala/org/ucombinator/jade/main/Main.scala
+++ decompiler/src/main/scala/org/ucombinator/jade/main/Main.scala
@@ -1,6 +1,5 @@
 package org.ucombinator.jade.main
 
-import java.io.IOException
 import java.util.concurrent.Callable
 import java.util.jar.Attributes
 
@@ -9,19 +8,30 @@ import picocli.CommandLine.{Command, HelpCommand, ParameterException, Parameters
 
 import scala.collection.JavaConverters._
 
-object Main extends App {
-  val commandLine: CommandLine = new CommandLine(new Main(commandLine))
-  System.exit(commandLine.execute(this.args:_*))
+// TODO: java -cp lib/jade/jade.jar picocli.AutoComplete -n jade org.ucombinator.jade.main.Main (see https://picocli.info/autocomplete.html)
+// TODO: description
+// TODO: header/footer?
+
+object Main {
+  val commandLine: CommandLine = new CommandLine(new Main())
+  def main(args: Array[String]): Unit = {
+    System.exit(commandLine.execute(args:_*))
+  }
 }
 
-// TODO: https://picocli.info/autocomplete.html
-// TODO: description
-// TODO: common super class instead of @ParentClass?
-// TODO: header/footer?
+// Class for common settings on commands
+// aliases, description, defaultValueProvider
+@Command(
+  mixinStandardHelpOptions = true,
+  requiredOptionMarker = '*',
+  showDefaultValues = true,
+  versionProvider = classOf[ManifestVersionProvider])
+abstract class Cmd[T] extends Callable[T] {
+  @ParentCommand var mainCommand: Main = _
+}
+
 @Command(
   name = "jade",
-  mixinStandardHelpOptions = true,
-  versionProvider = classOf[ManifestVersionProvider],
   subcommands = Array(
     classOf[HelpCommand],
     classOf[Decompile],
@@ -30,16 +40,15 @@ object Main extends App {
     classOf[JavaGrammarTypes],
     classOf[AsmInstructionConstants],
     classOf[AsmOpcodeConstants]))
-class Main(commandLine: => CommandLine) extends Callable[Unit] {
+class Main() extends Cmd[Unit] {
   override def call(): Unit = {
-    throw new ParameterException(this.commandLine, "Missing required parameter: [COMMAND]")
+    throw new ParameterException(Main.commandLine, "Missing required parameter: [COMMAND]")
   }
 }
 
 @Command(name="decompile")
-class Decompile extends Callable[Unit] {
-  @ParentCommand var main: Main = _
-
+class Decompile extends Cmd[Unit] {
+  // TODO: java.io.file or java.nio.path?
   @Parameters(paramLabel = "<file>", description = Array("The .class file to decompile")) // TODO: FILE?
   var fileName: String = _
 
@@ -48,11 +57,9 @@ class Decompile extends Callable[Unit] {
   }
 }
 
-@Command(name="java-grammar-download")
-class JavaGrammarDownload extends Callable[Unit] {
-  @ParentCommand var main: Main = _
-
-  @Parameters(paramLabel = "VERSION_OR_URL")
+@Command(name="java-grammar:download")
+class JavaGrammarDownload extends Cmd[Unit] {
+  @Parameters(paramLabel = "<version or url>")
   var versionOrUrl: String = _
 
   override def call(): Unit = {
@@ -60,68 +67,51 @@ class JavaGrammarDownload extends Callable[Unit] {
   }
 }
 
-@Command(name="java-grammar-extract")
-class JavaGrammarExtract extends Callable[Unit] {
-  @ParentCommand var main: Main = _
-
+@Command(name="java-grammar:extract")
+class JavaGrammarExtract extends Cmd[Unit] {
   override def call(): Unit = {
     extractGrammar.Main.main()
   }
 }
 
-@Command(name="java-grammar-types")
-class JavaGrammarTypes extends Callable[Unit] {
-  @ParentCommand var main: Main = _
-
+@Command(name="java-grammar:types")
+class JavaGrammarTypes extends Cmd[Unit] {
   override def call(): Unit = {
     javaFileTypes.Main.main()
   }
 }
 
-@Command(name="asm-instruction-constants")
-class AsmInstructionConstants extends Callable[Unit] {
-  @ParentCommand var main: Main = _
-
+@Command(name="asm-constants:instruction-types")
+class AsmInstructionConstants extends Cmd[Unit] {
   override def call(): Unit = {
     extractAsmTypes.Main.main()
   }
 }
 
-@Command(name="asm-opcode-constants")
-class AsmOpcodeConstants extends Callable[Unit] {
-  @ParentCommand var main: Main = _
-
+@Command(name="asm-constants:opcodes")
+class AsmOpcodeConstants extends Cmd[Unit] {
   override def call(): Unit = {
     extractAsmOpcodes.Main.main()
   }
 }
 
-/**
-  * {@link IVersionProvider} implementation that returns version information from the jar file's {@code /META-INF/MANIFEST.MF} file.
+/** [[picocli.CommandLine.IVersionProvider]] implementation that returns version information from the jar file's `/META-INF/MANIFEST.MF` file.
+  * Based on [[https://github.com/remkop/picocli/blob/master/picocli-examples/src/main/java/picocli/examples/VersionProviderDemo2.java]]
   */
 class ManifestVersionProvider extends CommandLine.IVersionProvider {
   @throws[Exception]
   override def getVersion: Array[String] = {
-    val resources = classOf[Main].getClassLoader.getResources("META-INF/MANIFEST.MF")
+    val manifestFile = "META-INF/MANIFEST.MF"
+    val title = "Jade"
+    val resources = classOf[Main].getClassLoader.getResources(manifestFile)
     for (url <- resources.asScala) {
-      try {
-        val manifest = new java.util.jar.Manifest(url.openStream)
-        if (isApplicableManifest(manifest)) {
-          val attr = manifest.getMainAttributes
-          return Array[String](f"${get(attr, "Implementation-Title")} version ${get(attr, "Implementation-Version")}")
-        }
-      } catch {
-        case ex: IOException =>
-          return Array[String]("Unable to read from " + url + ": " + ex)
+      val manifest = new java.util.jar.Manifest(url.openStream)
+      @inline def get(key: String) = manifest.getMainAttributes.get(new Attributes.Name(key))
+      // TODO: include versions of packaged jars?
+      if (title == get("Implementation-Title")) {
+        return Array[String](f"${get("Implementation-Title")} version ${get("Implementation-Version")}")
       }
     }
-    Array[String]()
+    throw new Exception(f"Unable to find `$manifestFile` with title '$title' from which to read version")
   }
-
-  private def isApplicableManifest(manifest: java.util.jar.Manifest) = {
-    val attributes = manifest.getMainAttributes
-    "Jade" == get(attributes, "Implementation-Title")
-  }
-
-  def get(attributes: Attributes, key: String) = attributes.get(new Attributes.Name(key))
 }
