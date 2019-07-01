ea08cbbb4483f65a2afce9afae92f7893dce615c
Michael D. Adams <https://michaeldadams.org/>
2019-07-01 04:29:20 -0600

    Renamed some things in VFS.scala

--- decompiler/src/main/scala/org/ucombinator/jade/util/VFS.scala
+++ decompiler/src/main/scala/org/ucombinator/jade/util/VFS.scala
@@ -27,11 +27,11 @@ case class RFile(bytes: Array[Byte]) extends ReadResult
 case object ROther extends ReadResult
 case object RNotExist extends ReadResult
 
-sealed trait ZipTree
-case class ZipTreeFile(bytes: Array[Byte]) extends ZipTree
-case class ZipTreeDirectory(children: SortedMap[String, ZipTree]) extends ZipTree
+sealed trait FileTree
+case class FileTreeFile(bytes: Array[Byte]) extends FileTree
+case class FileTreeDirectory(children: SortedMap[String, FileTree]) extends FileTree
 
-class PathPosition(basePath: Path, abovePath: List[String], belowPath: /*Reversed*/List[String], zipTree: Option[ZipTree]) {
+class PathPosition(basePath: Path, abovePath: List[String], belowPath: /*Reversed*/List[String], fileTree: Option[FileTree]) {
   assert(abovePath.isEmpty || belowPath.isEmpty)
   def apply[A](above: => A, same: => A, below: => A): A = {
     if (abovePath.nonEmpty) { above }
@@ -39,7 +39,7 @@ class PathPosition(basePath: Path, abovePath: List[String], belowPath: /*Reverse
     else { same }
   }
   def read: ReadResult = {
-    zipTree match {
+    fileTree match {
       case None =>
         val attributes = try {
           Files.readAttributes(path, classOf[BasicFileAttributes])
@@ -58,22 +58,22 @@ class PathPosition(basePath: Path, abovePath: List[String], belowPath: /*Reverse
         } else {
           ROther
         }
-      case Some(z) =>
-        z match {
-          case ZipTreeFile(bytes) => RFile(bytes)
-          case ZipTreeDirectory(children) =>
+      case Some(t) =>
+        t match {
+          case FileTreeFile(bytes) => RFile(bytes)
+          case FileTreeDirectory(children) =>
             abovePath match {
               case p :: ps =>
-                RDirectory(children.toList.filter(_._1 == p).map(zt => new PathPosition(basePath.resolve(zt._1), ps, belowPath, Some(zt._2))))
+                RDirectory(children.toList.filter(_._1 == p).map(ft => new PathPosition(basePath.resolve(ft._1), ps, belowPath, Some(ft._2))))
               case List() =>
-                RDirectory(children.toList.map(zt => new PathPosition(basePath, List(), zt._1 :: belowPath, Some(zt._2))))
+                RDirectory(children.toList.map(ft => new PathPosition(basePath, List(), ft._1 :: belowPath, Some(ft._2))))
             }
         }
     }
   }
   def path: Path = { belowPath.foldRight(basePath)((s,p) => p.resolve(s) ) }
   def parent: Option[PathPosition] = {
-    zipTree match {
+    fileTree match {
       case None =>
         assert(belowPath.isEmpty)
         val parent = path.getParent
@@ -82,8 +82,8 @@ class PathPosition(basePath: Path, abovePath: List[String], belowPath: /*Reverse
       case Some(_) => None
     }
   }
-  def withZipTree(zipTree: ZipTree): PathPosition = {
-    new PathPosition(basePath, abovePath, belowPath, Some(zipTree))
+  def withFileTree(fileTree: FileTree): PathPosition = {
+    new PathPosition(basePath, abovePath, belowPath, Some(fileTree))
   }
 }
 
@@ -111,28 +111,28 @@ object VFS {
   def ignore: Unit = {
     println("TODO: ignore")
   }
-  def add(path: List[String], zipTree: ZipTree, bytes: Array[Byte]): ZipTree = {
+  def add(path: List[String], fileTree: FileTree, bytes: Array[Byte]): FileTree = {
     path match {
       case List() =>
-        zipTree match {
-          case null => ZipTreeFile(bytes)
+        fileTree match {
+          case null => FileTreeFile(bytes)
           case _ => throw new Exception("TODO: bad add")
         }
       case p :: ps =>
-        zipTree match {
-          case ZipTreeFile(_) => throw new Exception("TODO: bad add 2")
-          case null => ZipTreeDirectory(SortedMap(p -> add(ps, null, bytes)))
-          case ZipTreeDirectory(children) =>
-            ZipTreeDirectory(children + (p -> add(ps, children.get(p).orNull, bytes)))
+        fileTree match {
+          case FileTreeFile(_) => throw new Exception("TODO: bad add 2")
+          case null => FileTreeDirectory(SortedMap(p -> add(ps, null, bytes)))
+          case FileTreeDirectory(children) =>
+            FileTreeDirectory(children + (p -> add(ps, children.get(p).orNull, bytes)))
         }
     }
   }
-  def readZip(bytes: Array[Byte], offset: Int = 0): ZipTree = {
+  def readZip(bytes: Array[Byte], offset: Int = 0): FileTree = {
     val zipInputStream = new ZipInputStream(new ByteArrayInputStream(bytes, offset, bytes.length - offset))
     // TODO: multi-version jar
     // NOTE: using ZipInputStream instead of JarInputStream so we can also handle `.jmod` files
     // NOTE: using ZipInputStream instead of ZipFile so we can also handle recursive zip/jar files
-    var zipTree: ZipTree = ZipTreeDirectory(SortedMap())
+    var fileTree: FileTree = FileTreeDirectory(SortedMap())
     val array = new Array[Byte](4 * 1024)
     var entry: ZipEntry = null
     while ({entry = zipInputStream.getNextEntry; entry != null}) {
@@ -143,13 +143,13 @@ object VFS {
           builder.write(array, 0, len)
         }
         // NOTE: officially the separator is "/", but some compression software uses "\"
-        zipTree = add(entry.getName.split(Array('/', '\\')).toList, zipTree, builder.toByteArray)
+        fileTree = add(entry.getName.split(Array('/', '\\')).toList, fileTree, builder.toByteArray)
       }
     }
     zipInputStream.close()
-    zipTree
+    fileTree
   }
-  def readJmod(bytes: Array[Byte]): ZipTree = { readZip(bytes, JMOD_OFFSET) }
+  def readJmod(bytes: Array[Byte]): FileTree = { readZip(bytes, JMOD_OFFSET) }
   var classes = Map[String, (String, ClassReader)]()
   def load(path: Path, bytes: Array[Byte]): Unit = {
     assert(bytes.startsWith(CLASS_SIGNATURE))
@@ -163,8 +163,8 @@ object VFS {
     path.read match {
       case RFile(bytes) =>
         if (bytes.startsWith(CLASS_SIGNATURE)) { path(error, load(path.path, bytes), load(path.path, bytes)) }
-        else if (bytes.startsWith(ZIP_SIGNATURE)) { get(path.withZipTree(readZip(bytes))) }
-        else if (bytes.startsWith(JMOD_SIGNATURE)) { get(path.withZipTree(readJmod(bytes))) }
+        else if (bytes.startsWith(ZIP_SIGNATURE)) { get(path.withFileTree(readZip(bytes))) }
+        else if (bytes.startsWith(JMOD_SIGNATURE)) { get(path.withFileTree(readJmod(bytes))) }
         else { path(error, error, ignore) }
       case RDirectory(children) =>
         // Note: one if zip, zero if fs or zip and not found or zip and directory is empty
