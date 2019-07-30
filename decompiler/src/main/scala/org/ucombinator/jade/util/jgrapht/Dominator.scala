848da97af40c164610841a5cad921b8459bb0f38
Michael D. Adams <https://michaeldadams.org/>
2019-07-29 22:35:56 -0600

    Some refactoring of the dominator code

--- decompiler/src/main/scala/org/ucombinator/jade/util/jgrapht/Dominator.scala
+++ decompiler/src/main/scala/org/ucombinator/jade/util/jgrapht/Dominator.scala
@@ -11,11 +11,11 @@ object Dominator {
 
   type DominatorTree[V] = Graph[V, Edge[V]]
 
-  // TODO: if missing node
-  // drawn from https://gist.github.com/yuzeh/a5e6602dfdb0db3c2130c10537db54d7
-  // paper: A Fast Algorithm for Finding Dominators in a Flowgraph, THOMAS LENGAUER and ROBERT ENDRE TARJAN
-  // ACM Transactions on Programming Languages and Systems, Vol. 1, No. 1, July 1979, Pages 121-141.
-  // https://eden.dei.uc.pt/~amilcar/pdf/CompilerInJava.pdf
+  // This implements the algorithm in the paper:
+  //   THOMAS LENGAUER and ROBERT ENDRE TARJAN. A Fast Algorithm for Finding Dominators in a Flowgraph.
+  //   ACM Transactions on Programming Languages and Systems, Vol. 1, No. 1, July 1979, Pages 121-141.
+  // Based on the code at https://gist.github.com/yuzeh/a5e6602dfdb0db3c2130c10537db54d7
+  // A useful description: https://eden.dei.uc.pt/~amilcar/pdf/CompilerInJava.pdf
   def dominatorTree[V >:Null <: AnyRef, E](graph: Graph[V, E], start: V): DominatorTree[V] = {
 
     // Original dealt in Ints, not Vs.
@@ -30,34 +30,16 @@ object Dominator {
       bucket(vertex) = Set.empty[V]
     }
 
-    val dfnum = mutable.Map.empty[V, Int] //order nodes reached in DFS
-    val vertex: mutable.ArraySeq[V] = new mutable.ArraySeq[V](numNodes) //vertex assigned a given number
-    val parent = mutable.Map.empty[V, V] //parent of node in DFS tree
-    val semi = mutable.Map.empty[V, V] //semidominaor of each V
-    val ancestor = mutable.Map.empty[V, V] //used for ancestorWithLowestSemi. Mutable, path compressed
-    val idom = mutable.Map.empty[V, V] //idom (once known)
-    val samedom = mutable.Map.empty[V, V] //node determined to have same idom
-    val best = mutable.Map.empty[V, V]//ancestor of V with lowest semidominator
+    val dfnum = mutable.Map.empty[V, Int] // The order of nodes reached in DFS
+    val vertex: mutable.ArraySeq[V] = new mutable.ArraySeq[V](numNodes) // The vertex assigned to a given number
+    val parent = mutable.Map.empty[V, V] // The parent of node in DFS tree
+    val semi = mutable.Map.empty[V, V] // The semidominaor of each V
+    val ancestor = mutable.Map.empty[V, V] // Used by ancestorWithLowestSemi. Mutable and path compressed.
+    val idom = mutable.Map.empty[V, V] // The idom (once known)
+    val samedom = mutable.Map.empty[V, V] // The node determined to have same idom
+    val best = mutable.Map.empty[V, V] // The ancestor of V with lowest semidominator
 
-    //Performs simple DFS, assign numbers and parents
-    def dfs(): Unit = {
-      var stack: List[(V, V)] = List((null: V, start))
-      while (stack.nonEmpty) {
-        val (p, n) = stack.head
-        stack = stack.tail
-        if (!dfnum.contains(n)) {
-          dfnum(n) = N
-          vertex(N) = n
-          parent(n) = p
-          N += 1
-          for (w <- successors(n)) {
-            stack = (n, w) :: stack
-          }
-        }
-      }
-    }
-
-    //Finds the ancestor of v with the lowest semidominator. Uses path compression to keep runtime down
+    // Finds the ancestor of v with the lowest semidominator. Uses path compression to keep runtime down.
     def ancestorWithLowestSemi(v: V): V = {
       val a = ancestor(v) //ancestor initially means parent; only modified here
       if (ancestor.contains(a)) { //if defined
@@ -70,29 +52,42 @@ object Dominator {
       best(v)
     }
 
-    //helper function: p is parent of n
+    // Helper function: p is parent of n
     def link(p: V, n: V): Unit = {
       ancestor(n) = p
       best(n) = n
     }
 
-    //Start of execution
-    dfs() //setup DFS tree
+    // Setup DFS tree
+    var stack: List[(V, V)] = List((null: V, start))
+    while (stack.nonEmpty) {
+      val (p, n) = stack.head
+      stack = stack.tail
+      if (!dfnum.contains(n)) {
+        dfnum(n) = N
+        vertex(N) = n
+        parent(n) = p
+        N += 1
+        for (w <- successors(n)) {
+          stack = (n, w) :: stack
+        }
+      }
+    }
 
+    // Iterate over nodes from bottom of DFS tree to top.
     for (i <- (N - 1) until 0 by -1) {
-      //Iterate over nodes from bottom of DFS tree to top.
       val n = vertex(i)
       val p = parent(n)
       var s = p
 
-      //find the semidominator of v
+      // Find the semidominator of v
       for (v <- predecessors(n)) {
-        val sPrime = if (dfnum(v) <= dfnum(n)) { //determines if pred is ancestor in DFS tree.
+        val sPrime = if (dfnum(v) <= dfnum(n)) { // Determine if pred is an ancestor in DFS tree.
           v
         } else {
           semi(ancestorWithLowestSemi(v))
         }
-        if (dfnum(sPrime) < dfnum(s)) { //picks lowest
+        if (dfnum(sPrime) < dfnum(s)) { // Pick lowest
           s = sPrime
         }
       }
@@ -102,7 +97,7 @@ object Dominator {
 
       link(p, n)
 
-      //for each bucket, find ancestor with lowest semi. If it has the same semi, that semi is the idom. If not, it has the same semidominator.
+      // For each bucket, find ancestor with lowest semi. If it has the same semi, that semi is the idom. If not, it has the same semidominator.
       for (v <- bucket(p)) {
         val y = ancestorWithLowestSemi(v)
         if (semi(y) == semi(v)) {
@@ -114,7 +109,7 @@ object Dominator {
       bucket(p) = Set.empty
     }
 
-    //iterate and assign idom based on samedom. Order guarantees idom will be defined in time.
+    // Iterate and assign idom based on samedom. Order guarantees idom will be defined in time.
     for (i <- 0 until N) {
       val n = vertex(i)
       if (samedom.contains(n)) {
@@ -122,7 +117,7 @@ object Dominator {
       }
     }
 
-    //algorithm complete; remaining code is just for translation to expected result structure
+    // Algorithm complete; remaining code is just for translation to expected result structure
     val tree = new SimpleDirectedGraph[V, Edge[V]](classOf[Edge[V]])
     tree.addVertex(start) //suspicious: may or may not be key in idom
     for ((key, value) <- idom) {
