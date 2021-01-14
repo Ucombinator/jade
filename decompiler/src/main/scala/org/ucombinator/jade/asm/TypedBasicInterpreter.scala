25f8e81c61583d309fc04578391e623b15c7c478
Michael D. Adams <adamsmd@users.noreply.github.com>
2021-01-14 11:14:49 -0500

    Format TypedBasicInterpreter

--- decompiler/src/main/scala/org/ucombinator/jade/asm/TypedBasicInterpreter.scala
+++ decompiler/src/main/scala/org/ucombinator/jade/asm/TypedBasicInterpreter.scala
@@ -5,10 +5,10 @@ import org.objectweb.asm.Type
 import org.objectweb.asm.tree.analysis.BasicInterpreter
 import org.objectweb.asm.tree.analysis.BasicValue
 
-/** A version of `BasicInterpreter` that gives actual types instead
- * of collapsing some types into BasicValue.REFERENCE_VALUE` ( array and object types)
- * or `BasicValue.INT_TYPE` (boolean, char, byte short, int).
- */
+/** A version of `BasicInterpreter` that gives actual types instead of
+  * collapsing some types into BasicValue.REFERENCE_VALUE` ( array and object
+  * types) or `BasicValue.INT_TYPE` (boolean, char, byte short, int).
+  */
 object TypedBasicInterpreter extends BasicInterpreter(Opcodes.ASM9) {
   override def newValue(`type`: Type): BasicValue = {
     if (`type` == null) {
